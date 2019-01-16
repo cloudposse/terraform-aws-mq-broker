12949e4555e751125d2d18a69abc30384aed8be3
Josh Myers <joshuajmyers@gmail.com>
2019-01-16 16:15:34 +0000

    Change output format due to Terraform issue
    Currently a plan is failing with lots of:
    
    ```
    * module.amq.output.secondary_ampq_ssl_endpoint: Resource 'aws_mq_broker.default' does not have attribute 'instances.1.endpoints.1' for variable 'aws_mq_broker.default.*.instances.1.endpoints.1'
    * module.amq.output.secondary_ip_address: Resource 'aws_mq_broker.default' does not have attribute 'instances.1.ip_address' for variable 'aws_mq_broker.default.*.instances.1.ip_address'
    * module.amq.output.primary_console_url: Resource 'aws_mq_broker.default' does not have attribute 'instances.0.console_url' for variable 'aws_mq_broker.default.*.instances.0.console_url'
    ```
    
    [1] https://github.com/hashicorp/terraform/issues/16681

--- outputs.tf
+++ outputs.tf
@@ -1,63 +1,63 @@
 output "broker_id" {
-  value = "${join("", aws_mq_broker.default.*.id)}"
+  value = "${element(concat(aws_mq_broker.default.*.id, list("")), 0)}"
 }
 
 output "broker_arn" {
-  value = "${join("", aws_mq_broker.default.*.arn)}"
+  value = "${element(concat(aws_mq_broker.default.*.arn, list("")), 0)}"
 }
 
 output "primary_console_url" {
-  value = "${join("", aws_mq_broker.default.*.instances.0.console_url)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.0.console_url, list("")), 0)}"
 }
 
 output "primary_ssl_endpoint" {
-  value = "${join("",aws_mq_broker.default.*.instances.0.endpoints.0i)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.0.endpoints.0, list("")), 0)}"
 }
 
 output "primary_ampq_ssl_endpoint" {
-  value = "${join("", aws_mq_broker.default.*.instances.0.endpoints.1)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.0.endpoints.1, list("")), 0)}"
 }
 
 output "primary_stomp_ssl_endpoint" {
-  value = "${join("", aws_mq_broker.default.*.instances.0.endpoints.2)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.0.endpoints.2, list("")), 0)}"
 }
 
 output "primary_mqtt_ssl_endpoint" {
-  value = "${join("", aws_mq_broker.default.*.instances.0.endpoints.3)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.0.endpoints.3, list("")), 0)}"
 }
 
 output "primary_wss_endpoint" {
-  value = "${join("", aws_mq_broker.default.*.instances.0.endpoints.4)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.0.endpoints.4, list("")), 0)}"
 }
 
 output "primary_ip_address" {
-  value = "${join("", aws_mq_broker.default.*.instances.0.ip_address)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.0.ip_address, list("")), 0)}"
 }
 
 output "secondary_console_url" {
-  value = "${join("", aws_mq_broker.default.*.instances.1.console_url)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.1.console_url, list("")), 0)}"
 }
 
 output "secondary_ssl_endpoint" {
-  value = "${join("", aws_mq_broker.default.*.instances.1.endpoints.0)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.1.endpoints.0, list("")), 0)}"
 }
 
 output "secondary_ampq_ssl_endpoint" {
-  value = "${join("", aws_mq_broker.default.*.instances.1.endpoints.1)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.1.endpoints.1, list("")), 0)}"
 }
 
 output "secondary_stomp_ssl_endpoint" {
-  value = "${join("", aws_mq_broker.default.*.instances.1.endpoints.2)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.1.endpoints.2, list("")), 0)}"
 }
 
 output "secondary_mqtt_ssl_endpoint" {
-  value = "${join("", aws_mq_broker.default.*.instances.1.endpoints.3)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.1.endpoints.3, list("")), 0)}"
 }
 
 output "secondary_wss_endpoint" {
-  value = "${join("", aws_mq_broker.default.*.instances.1.endpoints.4)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.1.endpoints.4, list("")), 0)}"
 }
 
 output "secondary_ip_address" {
-  value = "${join("", aws_mq_broker.default.*.instances.1.ip_address)}"
+  value = "${element(concat(aws_mq_broker.default.*.instances.1.ip_address, list("")), 0)}"
 }
