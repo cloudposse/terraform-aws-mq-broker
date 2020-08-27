f993b52329194cf649b55b33ad7b95d81220a288
Connor Bell (Makeshift) <git@connor-bell.com>
2020-08-27 18:27:05 +0100

    Update to TF0.13 syntax, optionally add to SSM, optionally provide or create KMS key, skip certain resources if alternatives provided by user

--- main.tf
+++ main.tf
@@ -1,131 +1,154 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.5.4"
-  namespace  = "${var.namespace}"
-  name       = "${var.name}"
-  stage      = "${var.stage}"
-  delimiter  = "${var.delimiter}"
-  attributes = "${var.attributes}"
-  tags       = "${var.tags}"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.0"
+  namespace  = var.namespace
+  name       = var.name
+  stage      = var.stage
+  delimiter  = var.delimiter
+  attributes = var.attributes
+  tags       = var.tags
 }
 
 locals {
-  kms_key_id              = "${length(var.kms_key_id) > 0 ? var.kms_key_id : format("alias/%s-%s-chamber", var.namespace, var.stage)}"
-  key_id                  = "${join("", data.aws_kms_key.chamber_kms_key.*.id)}"
-  chamber_service         = "${var.chamber_service == "" ? basename(pathexpand(path.module)) : var.chamber_service}"
-  mq_admin_user           = "${length(var.mq_admin_user) > 0 ? var.mq_admin_user : random_string.mq_admin_user.result}"
-  mq_admin_password       = "${length(var.mq_admin_password) > 0 ? var.mq_admin_password : random_string.mq_admin_password.result}"
-  mq_application_user     = "${length(var.mq_application_user) > 0 ? var.mq_application_user : random_string.mq_application_user.result}"
-  mq_application_password = "${length(var.mq_application_password) > 0 ? var.mq_application_password : random_string.mq_application_password.result}"
+  kms_key_id              = length(var.kms_key_id) > 0 ? var.kms_key_id : format("alias/%s-%s-chamber", var.namespace, var.stage)
+  create_key              = var.kms_key_id == "" && var.add_to_ssm ? true : false
+  key_id                  = length(var.kms_key_id) > 0 ? data.aws_kms_key.chamber_kms_key.0.id : var.add_to_ssm ? module.kms_key.0.key_id : ""
+  chamber_service         = var.chamber_service == "" ? basename(pathexpand(path.module)) : var.chamber_service
+  mq_admin_user           = length(var.mq_admin_user) > 0 ? var.mq_admin_user : random_string.mq_admin_user.0.result
+  mq_admin_password       = length(var.mq_admin_password) > 0 ? var.mq_admin_password : random_string.mq_admin_password.0.result
+  mq_application_user     = length(var.mq_application_user) > 0 ? var.mq_application_user : random_string.mq_application_user.0.result
+  mq_application_password = length(var.mq_application_password) > 0 ? var.mq_application_password : random_string.mq_application_password.0.result
 }
 
 data "aws_kms_key" "chamber_kms_key" {
-  key_id = "${local.kms_key_id}"
+  count = var.add_to_ssm && length(var.kms_key_id) > 0 ? 1 : 0
+  key_id = local.kms_key_id
+}
+
+module "kms_key" {
+  count = local.create_key ? 1 : 0
+  source                  = "git::https://github.com/cloudposse/terraform-aws-kms-key.git?ref=0.6.1"
+  namespace               = var.namespace
+  stage                   = var.stage
+  name                    = var.name
+  description             = "KMS key for chamber"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+  alias                   = local.kms_key_id
 }
 
 resource "random_string" "mq_admin_user" {
+  count = length(var.mq_admin_user) > 0 ? 0 : 1
   length  = 8
   special = false
   number  = false
 }
 
 resource "random_string" "mq_admin_password" {
+  count = length(var.mq_admin_password) > 0 ? 0 : 1
   length  = 16
   special = false
 }
 
 resource "random_string" "mq_application_user" {
+  count = length(var.mq_application_user) > 0 ? 0 : 1
   length  = 8
   special = false
   number  = false
 }
 
 resource "random_string" "mq_application_password" {
+  count = length(var.mq_application_password) > 0 ? 0 : 1
   length  = 16
   special = false
 }
 
 resource "aws_ssm_parameter" "mq_master_username" {
-  name        = "${format(var.chamber_parameter_name, local.chamber_service, "mq_admin_username")}"
-  value       = "${local.mq_admin_user}"
+  count = var.add_to_ssm ? 1 : 0
+  name        = format(var.chamber_parameter_name, local.chamber_service, "mq_admin_username")
+  value       = local.mq_admin_user
   description = "MQ Username for the master user"
   type        = "String"
-  overwrite   = "${var.overwrite_ssm_parameter}"
+  overwrite   = var.overwrite_ssm_parameter
 }
 
 resource "aws_ssm_parameter" "mq_master_password" {
-  name        = "${format(var.chamber_parameter_name, local.chamber_service, "mq_admin_password")}"
-  value       = "${local.mq_admin_password}"
+  count = var.add_to_ssm ? 1 : 0
+  name        = format(var.chamber_parameter_name, local.chamber_service, "mq_admin_password")
+  value       = local.mq_admin_password
   description = "MQ Password for the master user"
   type        = "SecureString"
-  key_id      = "${local.key_id}"
-  overwrite   = "${var.overwrite_ssm_parameter}"
+  key_id      = local.key_id
+  overwrite   = var.overwrite_ssm_parameter
 }
 
 resource "aws_ssm_parameter" "mq_application_username" {
-  name        = "${format(var.chamber_parameter_name, local.chamber_service, "mq_application_username")}"
-  value       = "${local.mq_application_user}"
+  count = var.add_to_ssm ? 1 : 0
+  name        = format(var.chamber_parameter_name, local.chamber_service, "mq_application_username")
+  value       = local.mq_application_user
   description = "AMQ username for the application user"
   type        = "String"
-  overwrite   = "${var.overwrite_ssm_parameter}"
+  overwrite   = var.overwrite_ssm_parameter
 }
 
 resource "aws_ssm_parameter" "mq_application_password" {
-  name        = "${format(var.chamber_parameter_name, local.chamber_service, "mq_application_password")}"
-  value       = "${local.mq_application_password}"
+  count = var.add_to_ssm ? 1 : 0
+  name        = format(var.chamber_parameter_name, local.chamber_service, "mq_application_password")
+  value       = local.mq_application_password
   description = "AMQ password for the application user"
   type        = "SecureString"
-  key_id      = "${local.key_id}"
-  overwrite   = "${var.overwrite_ssm_parameter}"
+  key_id      = local.key_id
+  overwrite   = var.overwrite_ssm_parameter
 }
 
 resource "aws_mq_broker" "default" {
-  broker_name                = "${module.label.id}"
-  deployment_mode            = "${var.deployment_mode}"
-  engine_type                = "${var.engine_type}"
-  engine_version             = "${var.engine_version}"
-  host_instance_type         = "${var.host_instance_type}"
-  auto_minor_version_upgrade = "${var.auto_minor_version_upgrade}"
-  apply_immediately          = "${var.apply_immediately}"
-  publicly_accessible        = "${var.publicly_accessible}"
-  security_groups            = ["${join("", aws_security_group.default.*.id)}"]
-  subnet_ids                 = ["${var.subnet_ids}"]
+  broker_name                = module.label.id
+  deployment_mode            = var.deployment_mode
+  engine_type                = var.engine_type
+  engine_version             = var.engine_version
+  host_instance_type         = var.host_instance_type
+  auto_minor_version_upgrade = var.auto_minor_version_upgrade
+  apply_immediately          = var.apply_immediately
+  publicly_accessible        = var.publicly_accessible
+  security_groups            = var.use_existing_security_groups == false ? [aws_security_group.default.0.id] : var.security_groups
+  subnet_ids                 = var.subnet_ids
 
   logs {
-    general = "${var.general_log}"
-    audit   = "${var.audit_log}"
+    general = var.general_log
+    audit   = var.audit_log
   }
 
   maintenance_window_start_time {
-    day_of_week = "${var.maintenance_day_of_week}"
-    time_of_day = "${var.maintenance_time_of_day}"
-    time_zone   = "${var.maintenance_time_zone}"
+    day_of_week = var.maintenance_day_of_week
+    time_of_day = var.maintenance_time_of_day
+    time_zone   = var.maintenance_time_zone
   }
 
-  user = [{
-    "username"       = "${local.mq_admin_user}"
-    "password"       = "${local.mq_admin_password}"
-    "groups"         = ["admin"]
-    "console_access" = true
-  }]
+  user {
+    username       = local.mq_admin_user
+    password       = local.mq_admin_password
+    groups         = ["admin"]
+    console_access = true
+  }
 
-  user = [{
-    "username" = "${local.mq_application_user}"
-    "password" = "${local.mq_application_password}"
-  }]
+  user {
+    username = local.mq_application_user
+    password = local.mq_application_password
+  }
 }
 
 resource "aws_security_group" "default" {
-  vpc_id = "${var.vpc_id}"
-  name   = "${module.label.id}"
-  tags   = "${module.label.tags}"
+  count = var.use_existing_security_groups == false ? 1 : 0
+  vpc_id = var.vpc_id
+  name   = module.label.id
+  tags   = module.label.tags
 }
 
 resource "aws_security_group_rule" "default" {
-  count                    = "${length(var.security_groups) > 0 ? length(var.security_groups) : 0}"
+  count                    = var.use_existing_security_groups == false && length(var.security_groups) > 0 ? length(var.security_groups) : 0
   type                     = "ingress"
   protocol                 = "tcp"
   from_port                = "0"
   to_port                  = "0"
-  source_security_group_id = "${element(var.security_groups, count.index)}"
-  security_group_id        = "${aws_security_group.default.id}"
+  source_security_group_id = element(var.security_groups, count.index)
+  security_group_id        = aws_security_group.default.0.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,89 +1,89 @@
 output "broker_id" {
-  value       = "${join("", aws_mq_broker.default.*.id)}"
+  value       = join("", aws_mq_broker.default.*.id)
   description = "AmazonMQ broker ID"
 }
 
 output "broker_arn" {
-  value       = "${join("", aws_mq_broker.default.*.arn)}"
+  value       = join("", aws_mq_broker.default.*.arn)
   description = "AmazonMQ broker ARN"
 }
 
 output "primary_console_url" {
-  value       = "${aws_mq_broker.default.instances.0.console_url}"
+  value       = aws_mq_broker.default.instances.0.console_url
   description = "AmazonMQ active web console URL"
 }
 
 output "primary_ssl_endpoint" {
-  value       = "${aws_mq_broker.default.instances.0.endpoints.0}"
+  value       = aws_mq_broker.default.instances.0.endpoints.0
   description = "AmazonMQ primary SSL endpoint"
 }
 
 output "primary_amqp_ssl_endpoint" {
-  value       = "${aws_mq_broker.default.instances.0.endpoints.1}"
+  value       = aws_mq_broker.default.instances.0.endpoints.1
   description = "AmazonMQ primary AMQP+SSL endpoint"
 }
 
 output "primary_stomp_ssl_endpoint" {
-  value       = "${aws_mq_broker.default.instances.0.endpoints.2}"
+  value       = aws_mq_broker.default.instances.0.endpoints.2
   description = "AmazonMQ primary STOMP+SSL endpoint"
 }
 
 output "primary_mqtt_ssl_endpoint" {
-  value       = "${aws_mq_broker.default.instances.0.endpoints.3}"
+  value       = aws_mq_broker.default.instances.0.endpoints.3
   description = "AmazonMQ primary MQTT+SSL endpoint"
 }
 
 output "primary_wss_endpoint" {
-  value       = "${aws_mq_broker.default.instances.0.endpoints.4}"
+  value       = aws_mq_broker.default.instances.0.endpoints.4
   description = "AmazonMQ primary WSS endpoint"
 }
 
 output "primary_ip_address" {
-  value       = "${aws_mq_broker.default.instances.0.ip_address}"
+  value       = aws_mq_broker.default.instances.0.ip_address
   description = "AmazonMQ primary IP address"
 }
 
 output "secondary_console_url" {
-  value       = "${aws_mq_broker.default.instances.1.console_url}"
+  value       = aws_mq_broker.default.instances.1.console_url
   description = "AmazonMQ secondary web console URL"
 }
 
 output "secondary_ssl_endpoint" {
-  value       = "${aws_mq_broker.default.instances.1.endpoints.0}"
+  value       = aws_mq_broker.default.instances.1.endpoints.0
   description = "AmazonMQ secondary SSL endpoint"
 }
 
 output "secondary_amqp_ssl_endpoint" {
-  value       = "${aws_mq_broker.default.instances.1.endpoints.1}"
+  value       = aws_mq_broker.default.instances.1.endpoints.1
   description = "AmazonMQ secondary AMQP+SSL endpoint"
 }
 
 output "secondary_stomp_ssl_endpoint" {
-  value       = "${aws_mq_broker.default.instances.1.endpoints.2}"
+  value       = aws_mq_broker.default.instances.1.endpoints.2
   description = "AmazonMQ secondary STOMP+SSL endpoint"
 }
 
 output "secondary_mqtt_ssl_endpoint" {
-  value       = "${aws_mq_broker.default.instances.1.endpoints.3}"
+  value       = aws_mq_broker.default.instances.1.endpoints.3
   description = "AmazonMQ secondary MQTT+SSL endpoint"
 }
 
 output "secondary_wss_endpoint" {
-  value       = "${aws_mq_broker.default.instances.1.endpoints.4}"
+  value       = aws_mq_broker.default.instances.1.endpoints.4
   description = "AmazonMQ secondary WSS endpoint"
 }
 
 output "secondary_ip_address" {
-  value       = "${aws_mq_broker.default.instances.1.ip_address}"
+  value       = aws_mq_broker.default.instances.1.ip_address
   description = "AmazonMQ secondary IP address"
 }
 
 output "admin_username" {
-  value       = "${local.mq_admin_user}"
+  value       = local.mq_admin_user
   description = "AmazonMQ admin username"
 }
 
 output "application_username" {
-  value       = "${local.mq_application_user}"
+  value       = local.mq_application_user
   description = "AmazonMQ application username"
 }
