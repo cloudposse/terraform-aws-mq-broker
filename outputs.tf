9af27b271a890eb83982cbbfc3084dfaad3452bb
treksler <treksler@users.noreply.github.com>
2021-03-18 15:33:31 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,72 +9,72 @@ output "broker_arn" {
 }
 
 output "primary_console_url" {
-  value       = aws_mq_broker.default[0].instances[0].console_url
+  value       = try(aws_mq_broker.default.*.instances[0].console_url, "")
   description = "AmazonMQ active web console URL"
 }
 
 output "primary_ssl_endpoint" {
-  value       = aws_mq_broker.default[0].instances[0].endpoints[0]
+  value       = try(aws_mq_broker.default.*.instances[0].endpoints[0], "")
   description = "AmazonMQ primary SSL endpoint"
 }
 
 output "primary_amqp_ssl_endpoint" {
-  value       = aws_mq_broker.default[0].instances[0].endpoints[1]
+  value       = try(aws_mq_broker.default.*.instances[0].endpoints[1], "")
   description = "AmazonMQ primary AMQP+SSL endpoint"
 }
 
 output "primary_stomp_ssl_endpoint" {
-  value       = aws_mq_broker.default[0].instances[0].endpoints[2]
+  value       = try(aws_mq_broker.default.*.instances[0].endpoints[2], "")
   description = "AmazonMQ primary STOMP+SSL endpoint"
 }
 
 output "primary_mqtt_ssl_endpoint" {
-  value       = aws_mq_broker.default[0].instances[0].endpoints[3]
+  value       = try(aws_mq_broker.default.*.instances[0].endpoints[3], "")
   description = "AmazonMQ primary MQTT+SSL endpoint"
 }
 
 output "primary_wss_endpoint" {
-  value       = aws_mq_broker.default[0].instances[0].endpoints[4]
+  value       = try(aws_mq_broker.default.*.instances[0].endpoints[4], "")
   description = "AmazonMQ primary WSS endpoint"
 }
 
 output "primary_ip_address" {
-  value       = aws_mq_broker.default[0].instances[0].ip_address
+  value       = try(aws_mq_broker.default.*.instances[0].ip_address, "")
   description = "AmazonMQ primary IP address"
 }
 
 output "secondary_console_url" {
-  value       = try(aws_mq_broker.default[0].instances[1].console_url, "")
+  value       = try(aws_mq_broker.default.*.instances[1].console_url, "")
   description = "AmazonMQ secondary web console URL"
 }
 
 output "secondary_ssl_endpoint" {
-  value       = try(aws_mq_broker.default[0].instances[1].endpoints[0], "")
+  value       = try(aws_mq_broker.default.*.instances[1].endpoints[0], "")
   description = "AmazonMQ secondary SSL endpoint"
 }
 
 output "secondary_amqp_ssl_endpoint" {
-  value       = try(aws_mq_broker.default[0].instances[1].endpoints[1], "")
+  value       = try(aws_mq_broker.default.*.instances[1].endpoints[1], "")
   description = "AmazonMQ secondary AMQP+SSL endpoint"
 }
 
 output "secondary_stomp_ssl_endpoint" {
-  value       = try(aws_mq_broker.default[0].instances[1].endpoints[2], "")
+  value       = try(aws_mq_broker.default.*.instances[1].endpoints[2], "")
   description = "AmazonMQ secondary STOMP+SSL endpoint"
 }
 
 output "secondary_mqtt_ssl_endpoint" {
-  value       = try(aws_mq_broker.default[0].instances[1].endpoints[3], "")
+  value       = try(aws_mq_broker.default.*.instances[1].endpoints[3], "")
   description = "AmazonMQ secondary MQTT+SSL endpoint"
 }
 
 output "secondary_wss_endpoint" {
-  value       = try(aws_mq_broker.default[0].instances[1].endpoints[4], "")
+  value       = try(aws_mq_broker.default.*.instances[1].endpoints[4], "")
   description = "AmazonMQ secondary WSS endpoint"
 }
 
 output "secondary_ip_address" {
-  value       = try(aws_mq_broker.default[0].instances[1].ip_address, "")
+  value       = try(aws_mq_broker.default.*.instances[1].ip_address, "")
   description = "AmazonMQ secondary IP address"
 }
 
