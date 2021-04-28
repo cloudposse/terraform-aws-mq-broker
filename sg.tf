18f21bc0366214cdae4433ba60409bcee1f437f7
heathsnow <hsnow@impinj.com>
2021-04-28 13:19:02 -0700

    ingress to_port 65535

--- sg.tf
+++ sg.tf
@@ -20,7 +20,7 @@ resource "aws_security_group_rule" "ingress_security_groups" {
   count                    = module.this.enabled && var.use_existing_security_groups == false ? length(var.allowed_security_groups) : 0
   description              = "Allow inbound traffic from existing Security Groups"
   from_port                = 0
-  to_port                  = 0
+  to_port                  = 65535
   protocol                 = "tcp"
   source_security_group_id = var.allowed_security_groups[count.index]
   security_group_id        = join("", aws_security_group.default.*.id)
@@ -30,8 +30,8 @@ resource "aws_security_group_rule" "ingress_security_groups" {
 resource "aws_security_group_rule" "ingress_cidr_blocks" {
   count             = module.this.enabled && var.use_existing_security_groups == false && length(var.allowed_cidr_blocks) > 0 ? 1 : 0
   description       = "Allow inbound traffic from CIDR blocks"
-  from_port         = "0"
-  to_port           = "0"
+  from_port         = 0
+  to_port           = 65535
   protocol          = "tcp"
   cidr_blocks       = var.allowed_cidr_blocks
   security_group_id = join("", aws_security_group.default.*.id)
