4d2194035bd580855904b95ba8c26c2b67aac956
Franck Cassedanne <franck@cassedanne.com>
2021-10-15 19:49:47 +0100

    Remove `hashicorp/null` (no longer used) & bump to >= 3.x `hashicorp/aws` & `hashicorp/random`.

--- versions.tf
+++ versions.tf
@@ -4,15 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.0"
+      version = ">= 3.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 2.2.0"
+      version = ">= 3.0"
     }
   }
 }
